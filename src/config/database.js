f46dc5beed6c03e1618d43e963bfacc7871ea5d1
Roberto Sousa <robertosousa186@gmail.com>
2022-02-15 20:22:07 -0300

    Creating config/database

---
+++ src/config/database.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const mongodbUrl = process.env.MONGODB_URL || 'localhost/test';
+
+try {
+  const connect = () =>
+    mongoose.connect(mongodbUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+  module.exports = {
+    connect,
+    connection: mongoose.connection,
+  };
+} catch (error) {
+  console.error('Error: ', error);
+}
