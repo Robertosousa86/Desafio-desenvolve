7c75f052c2a1652b77cd514a51704c8c76f9a423
Roberto Sousa <robertosousa186@gmail.com>
2022-02-16 15:16:21 -0300

    Using express methods

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const database = require('./config/database');
+const routes = require('./routes');
 
 const app = express();
+
 app.use(express.json());
 app.set('view-engine', 'ejs');
+app.use(express.static('views'));
+app.use(express.urlencoded({ extended: true }));
+
 // Configura o express e retornar uma nova instância da aplicação configurada.
 const configureExpress = () => {
   app.use('/', routes);
@@ -12,6 +17,7 @@ const configureExpress = () => {
 
   return app;
 };
+
 // Exporta uma função que retorna uma promise, assim que a promise for resolvida significa que o DB está disponível.
 module.exports = async () => {
   const app = configureExpress();
