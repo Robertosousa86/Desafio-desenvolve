42d21710325b5d17a16c1e6ccf0962ec7b77a699
Roberto Sousa <robertosousa186@gmail.com>
2022-02-16 15:14:53 -0300

    implementing first routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,21 @@
 // Index é responsável por carregar todas rotas da aplicação.
 const express = require('express');
+const registerController = require('./register');
 
 const router = express.Router();
 
+router.use('/register', registerController);
+
 router.get('/', (req, res) => {
-  res.render('index.ejs')
+  res.render('index.ejs');
+});
+
+router.get('/register', (req, res) => {
+  res.render('register.ejs');
+});
+
+router.post('/register', (req, res) => {
+  res.render('register.ejs');
 });
 
 module.exports = router;
-
